1c9f2d75af823fb25d50b3c2454e47b47dd417ff
MrSanderSi <sihversass@gmail.com>
2021-01-25 15:33:12 +0200

    flight display, flight sum and date filtering done

--- src/components/FlightList.vue
+++ src/components/FlightList.vue
@@ -7,21 +7,41 @@
             <td>Arrival</td>
             <td>Price</td>
         </tr>
+        <tr v-for="flight in flights" :key="flight.arrival">
+          <td>{{flight.from}}</td>
+          <td>{{flight.to}}</td>
+          <td><span>{{filteredDate(flight.departure)}}</span></td>
+          <td><span>{{filteredDate(flight.arrival)}}</span></td>
+          <td>{{flight.price}}</td>
+        </tr>
         <tr>
             <td colspan="3" style="text-align: right;">
-                <span>Total: <span id="total"></span> EUR</span>
+                <span>Total: <span id="total">{{total(flights)}}</span> EUR</span>
             </td>
         </tr>
     </table>
 </template>
 
 <script>
-
+    import moment from 'moment'
     export default {
         name: 'FlightList',
         props: {
             flights: Array
+        },
+        methods: {
+          total: function (flights) {
+            var total = [];
+            flights.forEach(element => { total.push(element.price)});
+            return total.reduce(function(total, sum){ return total + sum}, 0);
+          },
+          filteredDate: function(date){
+
+            return moment(date).format('MMM DD, YYYY hh:mm A');
+          }
+
         }
+
     }
 </script>
 
